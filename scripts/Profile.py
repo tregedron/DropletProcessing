9c7b38cb52410216804800c262d75406710c8c0d
tregedron <polovms@gmail.com>
2023-07-05 13:55:11 +0300

    added profile in liquid phases, reorganised some cycles to np

--- scripts/Profile.py
+++ scripts/Profile.py
@@ -1,5 +1,7 @@
-import numpy as np
+from utils.utils import custom_distance
+
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 
@@ -12,7 +14,7 @@ class Profile:
 
     def update_profile(self, droplet):
         '''
-
+        droplet can also be liquid phase...
         :return:
         '''
         self.num_of_averaging += 1
@@ -30,11 +32,11 @@ class Profile:
             self.profile[box[0], box[1], box[2]] += 1
 
     def save(self, path):
-        with open(os.path.join(path, 'profile.npy'), 'wb') as f:
+        with open(os.path.join(path, f'profile_{self.spacing}.npy'), 'wb') as f:
             np.save(f, self.profile / self.num_of_averaging)
 
-    def load(self, path):
-        with open(os.path.join(path, 'profile.npy'), 'rb') as f:
+    def load(self, path, name):
+        with open(os.path.join(path, name), 'rb') as f:
             self.profile = np.load(f)
             if self.spacing != self.profile.shape[0]:
                 print(self.profile.shape)
@@ -42,66 +44,118 @@ class Profile:
             else:
                 print(self.profile.shape)
 
-    def plot_profile(self, path):
-        print(self.num_of_averaging)
+    def plot_profile(self, path, name="voxel"):
         fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={"projection": "3d"})
+        fig.tight_layout()
         ax.voxels(self.profile, alpha=0.1)
 
         #label axes
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
-
+        # ortographic projection
         ax.set_proj_type('ortho')
 
         ax.view_init(0, 0)
-        fig.tight_layout()
-        plt.savefig(os.path.join(path, "voxel_0_0.png"), format="png", bbox_inches="tight")
+        plt.savefig(os.path.join(path, f"{name}_0_0.png"), format="png", bbox_inches="tight")
 
         ax.view_init(90, 0)
-        fig.tight_layout()
-        plt.savefig(os.path.join(path, "voxel_90_0.png"), format="png", bbox_inches="tight")
+        plt.savefig(os.path.join(path, f"{name}_90_0.png"), format="png", bbox_inches="tight")
 
         ax.view_init(0, 90)
-        fig.tight_layout()
-        plt.savefig(os.path.join(path, "voxel_0_90.png"), format="png", bbox_inches="tight")
+        plt.savefig(os.path.join(path, f"{name}_0_90.png"), format="png", bbox_inches="tight")
 
         ax.view_init(30, 30)
-        fig.tight_layout()
-        plt.savefig(os.path.join(path, "voxel_30_30.png"), format="png", bbox_inches="tight")
+        plt.savefig(os.path.join(path, f"{name}_30_30.png"), format="png", bbox_inches="tight")
 
         plt.close()
 
-    def process_profile_to_border(self, path):
-        avg_prof = self.profile / self.num_of_averaging
+    def process_profile_to_border(self, path, averaged=True):
+        if not averaged:
+            avg_prof = self.profile / self.num_of_averaging
+        else:
+            avg_prof = self.profile
 
         average = avg_prof[np.nonzero(avg_prof)].mean()
         print(avg_prof[np.nonzero(avg_prof)].shape)
 
-        border = np.where(avg_prof > 0.4 * average, avg_prof, 0)
-        border = np.where(border < 0.6 * average, border, 0)
+        border = np.where(avg_prof > 0.2 * average, avg_prof, 0)
+        border = np.where(border < 0.4 * average, border, 0)
 
-        print(border[np.nonzero(border)].shape)
+        with open(os.path.join(path, f'border_profile_{self.spacing}.npy'), 'wb') as f:
+            np.save(f, border)
 
-        fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={"projection": "3d"})
-        ax.voxels(border, alpha=0.1)
-        ax.set_proj_type('ortho')
+    def process_profile_g_of_r(self, path, averaged=True):
+        if not averaged:
+            avg_prof = self.profile / self.num_of_averaging
+        else:
+            avg_prof = self.profile
 
-        ax.view_init(90, 0)
+        center = np.array([self.spacing/2, self.spacing/2, self.spacing/2])
+        g_of_r = np.zeros(self.spacing)
+
+        for x in range(self.spacing):
+            for y in range(self.spacing):
+                for z in range(self.spacing):
+                    dr = np.round(np.sqrt((x - center[0])**2 + (y - center[1])**2 + (z - center[2])**2))
+                    dr = dr.astype(int)
+                    g_of_r[dr] += avg_prof[x, y, z]
+
+        N = avg_prof[np.nonzero(avg_prof)].sum()
+        V = avg_prof[np.nonzero(avg_prof)].shape[0]
+
+        for dist, layer in enumerate(g_of_r):
+            g_of_r[dist] = layer / (4 * np.pi * (dist+0.5)**2) * V / N
+
+        fig = plt.figure(figsize=(6, 6))
+        ax = fig.add_subplot(111)
+        ax.plot(g_of_r, 'b-', markersize=3)
+        plt.savefig(os.path.join(path, f'g(r).png'), bbox_inches='tight')
         fig.tight_layout()
-        plt.savefig(os.path.join(path, "border_90_0.png"), format="png", bbox_inches="tight")
-
-        ax.view_init(0, 0)
-        fig.tight_layout()
-        plt.savefig(os.path.join(path, "border_0_0.png"), format="png", bbox_inches="tight")
-        fig.tight_layout()
-
-        ax.view_init(30, 30)
-        fig.tight_layout()
-        plt.savefig(os.path.join(path, "border_30_30.png"), format="png", bbox_inches="tight")
-
+        plt.show()
         plt.close()
 
+
+    def process_profile_2d(self, path, name="", averaged=True):
+        if not averaged:
+            avg_prof = self.profile / self.num_of_averaging
+        else:
+            avg_prof = self.profile
+
+        center = np.array([self.spacing/2, self.spacing/2, self.spacing/2])
+        profile_rz = np.zeros((self.spacing, self.spacing))
+        for x in range(self.spacing):
+            for y in range(self.spacing):
+                for z in range(self.spacing):
+                    dr = np.round(np.sqrt((x - center[0])**2 + (y - center[1])**2))
+                    dr = dr.astype(int)
+                    profile_rz[dr, z] += avg_prof[x, y, z]
+
+        N = avg_prof[np.nonzero(avg_prof)].sum()
+        V = avg_prof[np.nonzero(avg_prof)].shape[0]
+
+        print(N, V)
+
+        # for Z in range(profile_rz.shape[1]):
+        #     for R in range(profile_rz.shape[0]):
+        #         profile_rz[R, Z] = profile_rz[R, Z] / (2 * np.pi * (R + 0.5)) * V / N
+
+        for R in range(profile_rz.shape[0]):
+            profile_rz[R, :] = profile_rz[R, :] / (2 * np.pi * (R+0.5)) * V / N
+
+        rho_max = np.max(profile_rz)
+
+        fig = plt.figure(figsize=(6, 6))
+        ax = fig.add_subplot(111)
+        fig.tight_layout()
+
+        ax.set_xlabel('R')
+        ax.set_ylabel('Z')
+
+        ax.pcolormesh(profile_rz.T, cmap='YlOrRd', vmin=0, vmax=rho_max)
+        plt.savefig(os.path.join(path, f'Rz_profile_{name}.png'), bbox_inches='tight')
+        plt.show()
+        plt.close()
 
 
 class ProfileOnFloor(Profile):
@@ -132,5 +186,26 @@ class ProfileOnFloor(Profile):
         for box in boxes:
             self.profile[box[0], box[1], box[2]] += 1
 
+class ProfileLiquidPhase(Profile):
+    # TODO here is a ЖЕСТЬ with python indexes usage... but it just works...
+    def __init__(self, spacing):
+        super().__init__(spacing)
 
+    def update_profile(self, droplet):
+        '''
+        droplet can also be liquid phase...
+        :return:
+        '''
+        self.num_of_averaging += 1
 
+        cell = np.array(droplet.cell)
+
+        distances = droplet.positions[:, :] - droplet.mass_center
+
+        distances = np.where(distances > 0.5 * cell, distances - cell, distances)
+        distances = np.where(distances < -1 * 0.5 * cell, distances + cell, distances)
+
+        boxes = np.round((distances/cell * self.spacing))
+        boxes = boxes.astype(int)
+        for box in boxes:
+            self.profile[box[0], box[1], box[2]] += 1
